034e7b23ced1ef6113f36d5bede6bef77f2a9340
wenqiL <wenqi.liang@mail.mcgill.ca>
2021-12-04 21:45:58 -0500

    Version 3
    User interface changed

--- LoRaMessenger/app/src/main/java/com/example/loramessenger/MainActivity.java
+++ LoRaMessenger/app/src/main/java/com/example/loramessenger/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.loramessenger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 //import androidx.lifecycle.ViewModelProvider;
 
 import com.example.loramessenger.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.jetbrains.annotations.Nullable;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 //import io.getstream.chat.android.client.ChatClient;
 //import io.getstream.chat.android.client.api.models.FilterObject;
@@ -29,86 +37,55 @@ import org.jetbrains.annotations.Nullable;
 
 public final class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> listitems = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    EditText input;
+    Button save;
+    ListView listView;
+    private BLEController bleController;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // Step 0 - inflate binding
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        input = findViewById(R.id.EnterDeviceName);
+        save = findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
 
-//        Button chat1 = (Button)findViewById(R.id.board1);
-//        Button chat2 = (Button)findViewById(R.id.board2);
-//        Button chat3 = (Button)findViewById(R.id.board3);
+        adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listitems);
+        listView.setAdapter(adapter);
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listitems.add(input.getText().toString());
+                adapter.notifyDataSetChanged();
+                input.setText("");
+            }
+        });
 
-//        chat1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, SecondActivity.class));
-//            }
-//        });
-//
-//        chat2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, ThirdActivity.class));
-//            }
-//        });
-//
-//        chat3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, FourthActivity.class));
-//            }
-//        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                }
 
+                if(position == 1){
+                    startActivity(new Intent(MainActivity.this, ThirdActivity.class));
+                }
 
-//        // Step 1 - Set up the client for API calls and the domain for offline storage
-//        ChatClient client = new ChatClient.Builder("b67pax5b2wdq", getApplicationContext())
-//                .logLevel(ChatLogLevel.ALL) // Set to NOTHING in prod
-//                .build();
-//        new ChatDomain.Builder(client, getApplicationContext()).build();
-//
-//        // Step 2 - Authenticate and connect the user
-//        User user = new User();
-//        user.setId("tutorial-droid");
-//        user.getExtraData().put("name", "Tutorial Droid");
-//        user.getExtraData().put("image", "https://bit.ly/2TIt8NR");
-//
-//        client.connectUser(
-//                user,
-//                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZHJvaWQifQ.NhEr0hP9W9nwqV7ZkdShxvi02C5PR7SJE7Cs4y7kyqg"
-//        ).enqueue();
-//
-//        // Step 3 - Set the channel list filter and order
-//        // This can be read as requiring only channels whose "type" is "messaging" AND
-//        // whose "members" include our "user.id"
-//        FilterObject filter = Filters.and(
-//                Filters.eq("type", "messaging"),
-//                Filters.in("members", singletonList(user.getId()))
-//        );
-//
-//        ChannelListViewModelFactory factory = new ChannelListViewModelFactory(
-//                filter,
-//                ChannelListViewModel.DEFAULT_SORT
-//        );
-//
-//        ChannelListViewModel channelsViewModel =
-//                new ViewModelProvider(this, factory).get(ChannelListViewModel.class);
-//
-//        // Step 4 - Connect the ChannelListViewModel to the ChannelListView, loose
-//        //          coupling makes it easy to customize
-//        ChannelListViewModelBinding.bind(channelsViewModel, binding.channelListView, this);
-//        binding.channelListView.setChannelItemClickListener(channel -> {
-//            // TODO - start channel activity
-//        });
+                if(position == 2){
+                    startActivity(new Intent(MainActivity.this, FourthActivity.class));
+                }
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        //menu.findItem(R.id.menu_refresh).setActionView(null);
 
         return true;
     }
@@ -120,9 +97,9 @@ public final class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_add) {
-            Intent mIntent = new Intent(this, AddDeviceActivity.class);
-            startActivity(mIntent);
+        if (id == R.id.action_dis) {
+            bleController.disconnect();
+            startActivity(new Intent(MainActivity.this, ConnectToDeviceActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
